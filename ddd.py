2d7802dda6baffaeee52ad1b80a1bea7dbe2b9c4
Killian Daly <daly.killian@gmail.com>
2016-04-27 22:19:45 +0200

    Clean up command line arguments
    add combine-command to assemble multiple .ddd files into one project.ddd

--- ddd.py
+++ ddd.py
@@ -16,10 +16,14 @@ def main():
     parser = ArgumentParser()
     subparsers = parser.add_subparsers(help='Supported Subcommands',dest='subcommand')
     parser_c = subparsers.add_parser('check', help='Check the DDD Project for consistency')
-    parser_c.add_argument(dest="dddfile", help="Filename of .ddd to add", metavar="dddfile", nargs='+')
+    parser_c.add_argument(dest="dddfile", help="Filename of .ddd to add", metavar="dddfile", nargs='?')
     parser_c.add_argument('--config',help='Config file', nargs='?')
-    parser_c.add_argument('--output',help='Output Filename', nargs='?')
     parser_c.add_argument('--conditions',help='Condition Filename', nargs='?')
+    
+    parser_co = subparsers.add_parser('combine', help='Check the DDD Project for consistency')
+    parser_co.add_argument(dest="dddfile", help="Filename of .ddd to add", metavar="dddfile", nargs='+')
+    parser_co.add_argument('--config',help='Config file', nargs='?')
+    parser_co.add_argument('--output',help='Output Filename', nargs='?')
     
     parser_v = subparsers.add_parser('view', help='Display the DDD Repository in a html page')
     parser_v.add_argument('--hash',help='Hash of the Object to check', nargs='?')
@@ -29,8 +33,6 @@ def main():
     parser_commit.add_argument(dest="dddfile", help="Filename of .ddd to add", metavar="dddfile", nargs='?')
     parser_commit.add_argument('--message',dest='message', help='Commit Message', nargs='?')
     parser_commit.add_argument('--tag',dest='tag', help='Tag', nargs='?')
-    
-    parser.add_argument(dest="paths", help="Path to root folder of DDD Repository", metavar="path", nargs=1)
     
     parser_export = subparsers.add_parser('export', help='Commit the DDD Object(s) to a local repository')
     parser_export.add_argument('--template',dest='template',help='Export the source for the current project')
@@ -43,26 +45,29 @@ def main():
     # Process arguments
     args = parser.parse_args()
     
-    paths = args.paths
     
-    db = DB(paths[0])
+    db = DB('./repo')
     
     status = 0
     
     if args.subcommand=='check':
+        data=db.open(args.dddfile)
+        cond = {}
+        if args.conditions:
+            with open(args.conditions,'r')as fp:
+                cond = json.load(fp)
+        status=db.check(data,conditions=cond)
+        return status
+        
+    elif args.subcommand=='combine':
         tmp = []
         for f in args.dddfile:
             tmp.append(db.open(f))
         
         c = db.open(args.config)
         proj = DddProject(components=tmp,config=c)
-        print args.conditions
-        with open(args.conditions,'r')as fp:
-            cond = json.load(fp)
-        status=db.check(proj,conditions=cond)
-        if not status:
-            db.dump(proj,args.output)
-        db.export_conditions(proj, 'conditions.dummy')
+        db.dump(proj,args.output)
+        
     elif args.subcommand=='view':
         db.view(name=args.name)
     elif args.subcommand=='commit':
@@ -71,11 +76,15 @@ def main():
         db.commit_and_tag(p, args.tag, args.message)
     elif args.subcommand=='export':
         data=db.open(args.dddfile)
-        config=db.open(args.config)
-        db.export_source(data=data,config=config, filename=args.output, template=args.template)
+        if args.config:
+            config=db.open(args.config)
+        if args.template=='conditions.json':
+            db.export_conditions(data, args.output)
+        else:
+            db.export_source(data=data,config=config, filename=args.output, template=args.template)
     elif args.subcommand=='init':
         for folder in ['index','objects','tags']:
-            os.makedirs(os.path.join(paths[0],folder))
+            os.makedirs(os.path.join('./repo',folder))
     return status
     
 if __name__ == "__main__":
